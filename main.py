459f4c4584685901dd475faf76cd9f4956917d56
2024kevinh <2024kevinh@gmail.com>
2025-09-15 00:14:22 +1200

    Finished cards of weapons and made a operators detail page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import sqlite3
 
 app = Flask(__name__)
@@ -16,62 +16,57 @@ def index():
 @app.route('/operators')
 def operators():
     conn = get_db_connection()
-    # Fetch operators with their abilities (multiple rows per operator possible)
     operators_data = conn.execute('''
         SELECT
             O.id,
             O.name,
             O.info,
             C.country AS country_name,
-            A.trait,
-            A.tactical_gear,
-            A.gadget1,
-            A.gadget2
+            O.trait,
+            O.tactical_gear,
+            O.gadget1,
+            O.gadget2
         FROM Operator AS O
         JOIN Country AS C ON O.country = C.id
-        LEFT JOIN Operator_Ability AS OA ON O.id = OA.operator_id
-        LEFT JOIN Ability AS A ON OA.ability_id = A.ability_id
     ''').fetchall()
     conn.close()
 
-    # Put in to order by op id
-    operators_dict = {}
+    operators_list = []
     for row in operators_data:
-        op_id = row["id"]
-
-        if op_id not in operators_dict:
-            operators_dict[op_id] = {
-                "id": row["id"],
-                "name": row["name"],
-                "info": row["info"],
-                "country_name": row["country_name"],
-                "trait": None,
-                "tactical_gear": None,
-                "gadget1": None,
-                "gadget2": None,
-                "abilities": []
-            }
-
+        operator = {
+            "id": row["id"],
+            "name": row["name"],
+            "info": row["info"],
+            "country_name": row["country_name"],
+            "trait": row["trait"],
+            "tactical_gear": row["tactical_gear"],
+            "gadget1": row["gadget1"],
+            "gadget2": row["gadget2"],
+            "abilities": []
+        }
         if row["trait"]:
-            operators_dict[op_id]["trait"] = row["trait"]
-            operators_dict[op_id]["abilities"].append(f"Trait: {row['trait']}")
+            operator["abilities"].append(f"Trait: {row['trait']}")
         if row["tactical_gear"]:
-            operators_dict[op_id]["tactical_gear"] = row["tactical_gear"]
-            operators_dict[op_id]["abilities"].append(f"Tactical Gear: {row['tactical_gear']}")
+            operator["abilities"].append(f"Tactical Gear: {row['tactical_gear']}")
         if row["gadget1"]:
-            operators_dict[op_id]["gadget1"] = row["gadget1"]
-            operators_dict[op_id]["abilities"].append(f"Gadget 1: {row['gadget1']}")
+            operator["abilities"].append(f"Gadget 1: {row['gadget1']}")
         if row["gadget2"]:
-            operators_dict[op_id]["gadget2"] = row["gadget2"]
-            operators_dict[op_id]["abilities"].append(f"Gadget 2: {row['gadget2']}")
-
-    operators_list = list(operators_dict.values())
+            operator["abilities"].append(f"Gadget 2: {row['gadget2']}")
+        operators_list.append(operator)
 
     return render_template('operators.html', operators=operators_list)
 
 @app.route('/weapons')
 def weapons():
-    return render_template('weapons.html')
+    categories = [
+        {"name": "assault_rifle", "label": "Assault Rifle", "image": "Assault Rifle.png"},
+        {"name": "submachine_gun", "label": "Submachine Gun", "image": "Submachine Gun.png"},
+        {"name": "shotgun", "label": "Shotgun", "image": "Shotgun.png"},
+        {"name": "lightmachine_gun", "label": "Lightmachine Gun", "image": "Lightmachine Gun.png"},
+        {"name": "marksman_rifle", "label": "Marksman Rifle", "image": "Marksman Rifle.png"},
+        {"name": "sniper_rifle", "label": "Sniper Rifle", "image": "Sniper Rifle.png"},
+    ]
+    return render_template('weapons.html', categories=categories)
 
 @app.route('/weapon/<int:weapon_id>')
 def weapon_detail(weapon_id):
@@ -86,6 +81,7 @@ def weapon_detail(weapon_id):
 
 @app.route('/weapons/<category>')
 def weapon_category(category):
+    prev_weapon_id = request.args.get('prev_weapon_id')
     conn = get_db_connection()
     # Map category to weapon ID ranges
     category_ranges = {
@@ -104,7 +100,7 @@ def weapon_category(category):
     else:
         weapons = []
     conn.close()
-    return render_template('weapon_category.html', weapons=weapons, category=category)
+    return render_template('weapon_category.html', weapons=weapons, category=category, prev_weapon_id=prev_weapon_id)
 
 @app.route('/operator/<int:operator_id>')
 def operator_detail(operator_id):
@@ -115,43 +111,35 @@ def operator_detail(operator_id):
             O.name,
             O.info,
             C.country AS country_name,
-            A.trait,
-            A.tactical_gear,
-            A.gadget1,
-            A.gadget2
+            O.trait,
+            O.tactical_gear,
+            O.gadget1,
+            O.gadget2
         FROM Operator AS O
         JOIN Country AS C ON O.country = C.id
-        LEFT JOIN Operator_Ability AS OA ON O.id = OA.operator_id
-        LEFT JOIN Ability AS A ON OA.ability_id = A.ability_id
         WHERE O.id = ?
-    ''', (operator_id,)).fetchall()
+    ''', (operator_id,)).fetchone()
     conn.close()
 
-    # Aggregate abilities for this operator
     operator = {
-        "id": row[0]["id"],
-        "name": row[0]["name"],
-        "info": row[0]["info"],
-        "country_name": row[0]["country_name"],
-        "trait": None,
-        "tactical_gear": None,
-        "gadget1": None,
-        "gadget2": None,
+        "id": row["id"],
+        "name": row["name"],
+        "info": row["info"],
+        "country_name": row["country_name"],
+        "trait": row["trait"],
+        "tactical_gear": row["tactical_gear"],
+        "gadget1": row["gadget1"],
+        "gadget2": row["gadget2"],
         "abilities": []
     }
-    for r in row:
-        if r["trait"]:
-            operator["trait"] = r["trait"]
-            operator["abilities"].append(f"Trait: {r['trait']}")
-        if r["tactical_gear"]:
-            operator["tactical_gear"] = r["tactical_gear"]
-            operator["abilities"].append(f"Tactical Gear: {r['tactical_gear']}")
-        if r["gadget1"]:
-            operator["gadget1"] = r["gadget1"]
-            operator["abilities"].append(f"Gadget 1: {r['gadget1']}")
-        if r["gadget2"]:
-            operator["gadget2"] = r["gadget2"]
-            operator["abilities"].append(f"Gadget 2: {r['gadget2']}")
+    if row["trait"]:
+        operator["abilities"].append(f"Trait: {row['trait']}")
+    if row["tactical_gear"]:
+        operator["abilities"].append(f"Tactical Gear: {row['tactical_gear']}")
+    if row["gadget1"]:
+        operator["abilities"].append(f"Gadget 1: {row['gadget1']}")
+    if row["gadget2"]:
+        operator["abilities"].append(f"Gadget 2: {row['gadget2']}")
 
     return render_template('operator_detail.html', operator=operator)
 
